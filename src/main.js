c621b322a500e786c66503c0080167f76bb5c6fb
Kit_Kat <matafeii38@gmail.com>
2026-07-07 00:19:05 +0300

    checkpoint: accepted current state

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './js/burgerMenu';
 import './js/heroImageToggle';
 import initAccordion from './js/accordion';
+import initRevealSections from './js/revealSections';
 import './js/swiper.js';
 initAccordion();
+initRevealSections();
